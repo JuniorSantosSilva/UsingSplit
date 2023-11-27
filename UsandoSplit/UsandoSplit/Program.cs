1741b63d05d3a4ed8fa61e25d073c4e090d7e4d5
Junior <51051265+JuniorSantosSilva@users.noreply.github.com>
2023-11-27 14:21:37 -0300

    Add files via upload

---
+++ UsandoSplit/UsandoSplit/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UsandoSplit
+{
+internal class Program {
+
+        static void Main(string[] args) {
+
+
+            Console.WriteLine("Digite 3 cores: ");
+
+
+            string s = Console.ReadLine();
+
+            string[] v = s.Split(' ');
+            string a = v[0];
+            string b = v[1];
+            string c = v[2];
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+
+        }
+
+
+    }
+}
